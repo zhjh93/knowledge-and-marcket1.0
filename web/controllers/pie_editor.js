9781db12ba6df7dd1be9d5abe1770f5b4b620507
zhyuzh <zhyuzh3d@hotmail.com>
2016-08-10 11:15:04 +0800

    editor完成布局

--- web/controllers/pie_editor.js
+++ web/controllers/pie_editor.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
     var thisName = 'pie_editor';
 
@@ -19,11 +19,43 @@
 
 
         //锚点
-        $scope.goto = function(key) {
+        $scope.goto = function (key) {
             $location.hash(key);
             $anchorScroll();
         };
 
+        //实时屏幕尺寸
+        $scope.sizeGt = function (str) {
+            return $mdMedia('gt-' + str);
+        }
+
+        //自动隐藏list和preview
+        $scope.layoutInit = function () {
+            if (!$mdMedia('gt-xs')) {
+                $scope.hideList = true;
+            };
+            if (!$mdMedia('gt-sm')) {
+                $scope.hidePreview = true;
+            }
+        }
+        $scope.layoutInit();
+
+        //低于xs各个部分solo显示
+        $scope.tagPart = function (str) {
+            if (!$mdMedia('gt-xs')) {
+                $scope.hideList = true;
+                $scope.hideEditor = true;
+                $scope.hidePreview = true;
+                $scope[str] = !$scope[str];
+            } else {
+                $scope[str] = !$scope[str];
+            }
+        };
+
+
+
+
+        //
 
         $scope.appName = $scope.xargs.app;
 
